5a5df6d9e6384fa6b45051b726cbf899f5723b75
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-11-08 17:01:38 +1100

    Add moveKnight recursive function
    This is the function that will handle moving the
    knight to new vertices and pushing the destinations
    to a path array as it goes. At the moment it works
    at finding a path to 3,3 from 0,0 but there's an
    error occuring at line 48 in the filterArray
    function when it gets too close to the edge of the
    board

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+let positionsVisited = [];
+let queue = [];
+
 function knightMoves(start, finish) {
   let currentPosition = start;
 }
 
-function returnValidMoves(currentPosition, previousPositions) {
+function returnValidMoves(currentPosition, previousPositions = []) {
   let [x, y] = currentPosition;
   let validMoves = filterMoves(
     calculateMoves(currentPosition),
@@ -61,4 +64,55 @@ function filterMoves(array, previousMovesArray = []) {
   return filteredArray;
 }
 
-console.log(returnValidMoves([3, 3], [[2, 1]]));
+// function hop(currentPosition, finalPosition, validMoveArray) {
+//   let vertex = currentPosition;
+//   while (vertex[0] !== finalPosition[0] && vertex[1] !== finalPosition[1]) {
+//     positionsVisited.push(vertex);
+//     console.log(positionsVisited);
+//     for (let move of validMoveArray) {
+//       vertex = returnValidMoves(move, positionsVisited);
+//     }
+//   }
+//   console.log("final destination reached");
+//   return vertex;
+// }
+
+function moveKnight(start, end) {
+  positionsVisited.push(start);
+  // Delete these later, just seeing what's going on
+  console.log(`--- Entered moveKnight function ---`);
+  console.log(`Our start value is: ${start} | Our end value is: ${end}`);
+  console.log(`At this point in time positionsVisited is:`);
+  console.log(positionsVisited);
+
+  // base case
+  if (start[0] === end[0] && start[1] === end[1]) {
+    console.log("Yay we made it! 😃");
+    console.log(`Our path was:`);
+    console.log(positionsVisited);
+    console.log(`Which was reached in ${positionsVisited.length} steps`);
+    console.log(`⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️`);
+    return;
+  }
+
+  // recursive case
+  queue.push(start);
+  let validMoves = returnValidMoves(start, positionsVisited);
+  console.log(`********** Valid moves are **********`);
+  console.log(validMoves);
+  console.log(`*************************************`);
+  // If, within the list of valid moves we find our end destination, recall the function to reach the base case
+  for (let move of validMoves) {
+    if (move[0] === end[0] && move[1] === end[1]) {
+      moveKnight([move[0], move[1]], end);
+    }
+  }
+  // Otherwise, for each valid move of valid moves recall moveKnight
+  for (let move of validMoves) {
+    moveKnight([move[0], move[1]], end);
+  }
+}
+
+// console.log(returnValidMoves([6, 3], [[4, 2]]));
+// hop([0, 0], [7, 7], returnValidMoves([0, 0]));
+moveKnight([0, 0], [3, 3]);
